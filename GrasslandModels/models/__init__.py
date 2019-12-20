aecaef1f34e4706d2ad649beab03ba257ce3e252
Shawn Taylor <shawntaylor@weecology.org>
2019-12-20 11:22:54 -0700

    working numpy versions of the choler 2011 models

--- GrasslandModels/models/__init__.py
+++ GrasslandModels/models/__init__.py
@@ -1,3 +1,8 @@
 from .phenograss import PhenoGrass
+from .choler import CholerPR1, CholerPR2, CholerPR3
 
-__all__ = ['PhenoGrass']
+__all__ = ['PhenoGrass',
+           'CholerPR1',
+           'CholerPR2',
+           'CholerPR3',
+           ]
