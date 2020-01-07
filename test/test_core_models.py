ff00a214d5170ef83b1842703a912b07279212de
Shawn Taylor <shawntaylor@weecology.org>
2020-01-07 13:36:01 -0700

    add some generic tests for new models

--- test/test_core_models.py
+++ test/test_core_models.py
@@ -2,17 +2,43 @@ from GrasslandModels import models, utils
 import pytest
 import numpy as np
 
-
+core_model_names = ['PhenoGrass','CholerPR3','CholerPR3','CholerPR3']
 
 GCC, predictor_vars = utils.load_test_data()
 
-quick_testing_params = {'maxiter':5,
-                         'popsize':3,
+quick_testing_params = {'maxiter':3,
+                         'popsize':2,
                          'mutation':(0.5,1),
                          'recombination':0.25,
                          'disp':False}
 
 
+
+# Setup a list of test cases where each one = (model_name, fitted_model object)
+fitted_models = []
+for name in core_model_names:
+    m = utils.load_model(name)()
+    this_model_data = {k:predictor_vars[k] for k in m._required_predictors.keys()}
+    m.fit(GCC, this_model_data, optimizer_params=quick_testing_params)
+    fitted_models.append(m)
+
+model_test_cases = list(zip(core_model_names, fitted_models))
+
+#######################################################################
+
+@pytest.mark.parametrize('model_name, fitted_model', model_test_cases)
+def test_predict_output_length(model_name, fitted_model):
+    """Predict output shape should equal input shape"""
+    assert fitted_model.predict().shape == GCC.shape
+
+
+@pytest.mark.parametrize('model_name, fitted_model', model_test_cases)
+def test_score(model_name, fitted_model):
+    """Score should return a single number"""
+    assert isinstance(fitted_model.score(), np.floating)
+
+########################################################################
+# Some PhenoGrass specific tests
 original_phenograss_params = {'b1':124.502121,
                            'b2':0.00227958267,
                            'b3':0.0755224228,
