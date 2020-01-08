36d770aa38aacfaddeb1fa2eef766df5e198c374
Shawn Taylor <sdtaylor@users.noreply.github.com>
2020-01-08 11:52:53 -0700

    add quick load of prefit models (#16)
    - These are the models parameterized with the parameters specified in the
    original papers. Should be usefull in testing/comparison stuff.
    
    - replaces the phenograss parameter loading in the tests

--- test/test_core_models.py
+++ test/test_core_models.py
@@ -41,19 +41,14 @@ def test_score(model_name, fitted_model):
     """Score should return a single number"""
     assert isinstance(fitted_model.score(), np.floating)
 
+def test_prefit_model_loading():
+    """Ensure prefit models are loading correctly"""
+    prefit_models = ['CholerPR1-original','CholerPR2-original','PhenoGrass-original']
+    models = [utils.load_prefit_model(n) for n in prefit_models]
+    assert all([isinstance(m.get_params(), dict) for m in models])
+    
 ########################################################################
 # Some PhenoGrass specific tests
-original_phenograss_params = {'b1':124.502121,
-                           'b2':0.00227958267,
-                           'b3':0.0755224228,
-                           'b4':0.519348383,
-                           #'L':2.4991734,
-                           'L':2,
-                           'Phmin':8.14994431,
-                           'h': 222.205673,
-                           'Topt':33.3597641,
-                           'Phmax':37.2918091}
-
 def test_phenograss_fit():
     m = models.PhenoGrass(parameters= {'L':2})
     m.fit(GCC, predictor_vars, optimizer_params=quick_testing_params, debug = True)
@@ -61,10 +56,10 @@ def test_phenograss_fit():
 
 
 def test_phenograss_internal_methods():
-    m1 = models.PhenoGrass(parameters = original_phenograss_params)
+    m1 = utils.load_prefit_model('PhenoGrass-original')
     m1.set_internal_method(method='cython')
     
-    m2 = models.PhenoGrass(parameters = original_phenograss_params)
+    m2 = utils.load_prefit_model('PhenoGrass-original')
     m2.set_internal_method('numpy')
     
     m1_prediction = m1.predict(predictor_vars)
